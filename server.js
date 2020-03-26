e5792d3adaa6572a8607baa1121336f1bd9025f2
Neslanovic Timea <timy1901@gmail.com>
2020-03-26 17:13:31 +0100

    post and del added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const mount = require('koa-mount');
 const router = require('koa-route');
 const { createApolloFetch } = require('apollo-fetch');
+const bodyParser = require('koa-bodyparser');
 
 const schema = require('./graphql/schema');
 const root = require('./graphql/root');
@@ -16,7 +17,7 @@ const fetch = createApolloFetch({
 mongoose.connect('mongodb://localhost:27017/calendar', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: true
+  useFindAndModify: false
 });
 
 const db = mongoose.connection
@@ -24,6 +25,15 @@ db.on('error', console.error.bind( console, 'conection error:'));
 db.once('open', ()=> console.log('Database connected'));
 
 const app = new koa();
+// app.use(bodyParser());
+
+app.use(
+  mount('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  }))
+);
 
 // Routing
 app.use(router.get('/', async ctx => {
@@ -66,23 +76,58 @@ app.use(router.get('/:id', async (ctx, next) => {
   
 }));
 
-// app.use(router.post('/', async ctx => {
-//   console.log(ctx.req);
+app.use(router.post('/', async ctx => {
+  const reqBody = ctx.request.body
+  console.log(reqBody.title);
+  await fetch({ 
+    query: ` mutation{
+      createEvent(title: "${reqBody.title}", start: "${reqBody.start}", end: "${reqBody.end}", allDay: ${reqBody.allDay}) 
+          {
+              id
+              title
+              allDay
+              start
+              end
+          }
+  }`})
+    .then(res => {
+      ctx.body = res.data
+    })
+}));
+
+// app.use(router.put('/:id', async ctx => {
+//   const paramID = ctx.request.originalUrl.replace('/','')
+//   // const reqBody = ctx.req.body
+//   // console.log(reqBody);
+//   console.log(ctx.request);
 //   await fetch({ 
-//     query: ` mutation{
-//       createEvent(title: $title, start: $start, end: $end) 
-//           {
-//               id
-//               title
-//               allDay
-//               start
-//               end
-//           }
+//     query: `
+//     mutation {
+//       updateEvent(id:"${paramID}", title: "Valami"){
+//           id
+//           title
+//           allDay
+//           start
+//           end
+//       }
 //   }`})
 //     .then(res => {
+//       console.log(res.data);
 //       ctx.body = res.data
 //     })
 // }));
+
+app.use(router.del('/:id', async ctx => {
+  const paramID = ctx.request.originalUrl.replace('/','')
+  await fetch({ 
+    query: `
+    mutation {
+      deleteEvent(id:"${paramID}")
+  }`})
+    .then(res => {
+      ctx.body = res.data
+    })
+}));
 
 
 app.listen(9000);
@@ -91,10 +136,4 @@ app.on('error', err => {
   console.log('server error', err);
 });
 
-app.use(
-  mount('/graphql', graphqlHTTP({
-    schema,
-    rootValue: root,
-    graphiql: true
-  }))
-);
+
