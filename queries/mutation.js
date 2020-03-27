de1b7d5406173ffb8def0dfd155216f68178717f
Neslanovic Timea <timy1901@gmail.com>
2020-03-27 11:23:45 +0100

    mutation.js added

---
+++ queries/mutation.js
@@ -0,0 +1,6 @@
+module.exports = {
+    delete: `
+    mutation($id: String!) {
+      deleteEvent(id:$id")
+  }`
+}
--- routes/event_routes.js
+++ routes/event_routes.js
@@ -3,7 +3,8 @@ const { createApolloFetch } = require('apollo-fetch');
 const uri = 'http://localhost:9000/graphql';
 const fetch = createApolloFetch({ uri });
 const router = new Router(); 
-const query = require('../queries/query') 
+const query = require('../queries/query');
+const mutation = require('../queries/mutation'); 
 
 router
     .get('/', async (ctx, next) => {
@@ -24,24 +25,29 @@ router
             })
         
         })
-    .post('/', async ctx => {
-        const reqBody = ctx.request.body
-        console.log(reqBody.title);
-        await fetch({ 
-            query: ` mutation{
-            createEvent(title: "${reqBody.title}", start: "${reqBody.start}", end: "${reqBody.end}", allDay: ${reqBody.allDay}) 
-                {
-                    id
-                    title
-                    allDay
-                    start
-                    end
-                }
-        }`})
-            .then(res => {
-            ctx.body = res.data
-            })
-    })
+    // .post('/', async (ctx, next) => {
+    //     const reqBody = ctx.request.body
+    //     console.log(reqBody);
+    //     await fetch({ 
+    //         query: `mutation ( $title: String!, $start: String, $end: String, $allDay: Boolean){
+    //         createEvent(title: $title, start: $start, end: $end, allDay: $allDay) 
+    //             {
+    //                 id
+    //                 title
+    //                 allDay
+    //                 start
+    //                 end
+    //             }
+    //     }`
+    //     , variables: {
+    //       title: reqBody.title,
+    //       start: reqBody.start,
+    //       end: reqBody.end,
+    //       allDay:reqBody.allDay}})
+    //         .then(res => {
+    //         ctx.body = res.data
+    //         })
+    // })
     
 
 .put('/:id', async ctx => {
@@ -65,13 +71,15 @@ router
       ctx.body = res.data
     })
 })
-.del('/:id', async ctx => {
-  const paramID = ctx.request.originalUrl.replace('/','')
+.del('/:id', async (ctx, next) => {
+  // const paramID = ctx.request.originalUrl.replace('/','')
   await fetch({ 
-    query: `
+    // query: mutation.delete,
+    query:`
     mutation {
       deleteEvent(id:"${paramID}")
-  }`})
+  }`
+    // variables: { id: ctx.params.id}})
     .then(res => {
       ctx.body = res.data
     })
